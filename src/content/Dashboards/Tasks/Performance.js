9f4327ba1dc51e94186525cd71932e7a134bd911
Rossi Kamal <rossikamal@gmail.com>
2022-07-19 21:08:48 +0600

    Update Performance.js

--- src/content/Dashboards/Tasks/Performance.js
+++ src/content/Dashboards/Tasks/Performance.js
@@ -1,142 +1,143 @@
-import {
-  Card,
-  Box,
-  CardContent,
-  Typography,
-  Avatar,
-  useTheme,
-  LinearProgress,
-  styled
-} from '@mui/material';
-import AssignmentTurnedInTwoToneIcon from '@mui/icons-material/AssignmentTurnedInTwoTone';
-import CancelPresentationTwoToneIcon from '@mui/icons-material/CancelPresentationTwoTone';
-
-const RootWrapper = styled(Card)(
-  ({ theme }) => `
-    background: ${theme.colors.gradients.green1};
-    color: ${theme.colors.alpha.white[100]};
-`
-);
-
-const AvatarSuccess = styled(Avatar)(
-  ({ theme }) => `
-      background-color: ${theme.colors.alpha.white[100]};
-      color: ${theme.palette.success.contrastText};
-      width: ${theme.spacing(8)};
-      height: ${theme.spacing(8)};
-      box-shadow: ${theme.colors.shadows.success};
-`
-);
-
-const AvatarError = styled(Avatar)(
-  ({ theme }) => `
-      background-color: ${theme.colors.error.main};
-      color: ${theme.palette.error.contrastText};
-      width: ${theme.spacing(8)};
-      height: ${theme.spacing(8)};
-      box-shadow: ${theme.colors.shadows.error};
-`
-);
-
-const TypographySecondary = styled(Typography)(
-  ({ theme }) => `
-      color: ${theme.colors.alpha.white[70]};
-`
-);
-
-const LinearProgressWrapper = styled(LinearProgress)(
-  ({ theme }) => `
-        flex-grow: 1;
-        margin-right: ${theme.spacing(1)};
-        height: 10px;
+ import {
+    Card,
+    Box,
+    CardContent,
+    Typography,
+    Avatar,
+    useTheme,
+    LinearProgress,
+    styled
+  } from '@mui/material';
+  import AssignmentTurnedInTwoToneIcon from '@mui/icons-material/AssignmentTurnedInTwoTone';
+  import CancelPresentationTwoToneIcon from '@mui/icons-material/CancelPresentationTwoTone';
+  
+  const RootWrapper = styled(Card)(
+    ({ theme }) => `
+      background: ${theme.colors.gradients.green1};
+      color: ${theme.colors.alpha.white[100]};
+  `
+  );
+  
+  const AvatarSuccess = styled(Avatar)(
+    ({ theme }) => `
+        background-color: ${theme.colors.alpha.white[100]};
+        color: ${theme.palette.success.contrastText};
+        width: ${theme.spacing(8)};
+        height: ${theme.spacing(8)};
+        box-shadow: ${theme.colors.shadows.success};
+  `
+  );
+  
+  const AvatarError = styled(Avatar)(
+    ({ theme }) => `
         background-color: ${theme.colors.error.main};
-
-        .MuiLinearProgress-barColorPrimary {
-          background-color: ${theme.colors.alpha.white[100]};
-          border-top-right-radius: ${theme.general.borderRadius};
-          border-bottom-right-radius: ${theme.general.borderRadius};
-        }
-`
-);
-
-function Performance() {
-  const theme = useTheme();
-
-  return (
-    <RootWrapper
-      sx={{
-        p: 2
-      }}
-    >
-      <Typography
-        variant="h3"
+        color: ${theme.palette.error.contrastText};
+        width: ${theme.spacing(8)};
+        height: ${theme.spacing(8)};
+        box-shadow: ${theme.colors.shadows.error};
+  `
+  );
+  
+  const TypographySecondary = styled(Typography)(
+    ({ theme }) => `
+        color: ${theme.colors.alpha.white[70]};
+  `
+  );
+  
+  const LinearProgressWrapper = styled(LinearProgress)(
+    ({ theme }) => `
+          flex-grow: 1;
+          margin-right: ${theme.spacing(1)};
+          height: 10px;
+          background-color: ${theme.colors.error.main};
+  
+          .MuiLinearProgress-barColorPrimary {
+            background-color: ${theme.colors.alpha.white[100]};
+            border-top-right-radius: ${theme.general.borderRadius};
+            border-bottom-right-radius: ${theme.general.borderRadius};
+          }
+  `
+  );
+  
+  function Performance() {
+    const theme = useTheme();
+  
+    return (
+      <RootWrapper
         sx={{
-          px: 2,
-          pb: 1,
-          pt: 2,
-          fontSize: `${theme.typography.pxToRem(23)}`,
-          color: `${theme.colors.alpha.white[100]}`
+          p: 2
         }}
       >
-        Performance
-      </Typography>
-      <CardContent>
-        <Box
-          display="flex"
+        <Typography
+          variant="h3"
           sx={{
             px: 2,
-            pb: 3
+            pb: 1,
+            pt: 2,
+            fontSize: `${theme.typography.pxToRem(23)}`,
+            color: `${theme.colors.alpha.white[100]}`
           }}
-          alignItems="center"
         >
-          <AvatarSuccess
+         Post View Statistics
+        </Typography>
+        <CardContent>
+          <Box
+            display="flex"
             sx={{
-              mr: 2
+              px: 2,
+              pb: 3
             }}
-            variant="rounded"
+            alignItems="center"
           >
-            <AssignmentTurnedInTwoToneIcon fontSize="large" />
-          </AvatarSuccess>
-          <Box>
-            <Typography variant="h1">23</Typography>
-            <TypographySecondary variant="subtitle2" noWrap>
-              tasks created
-            </TypographySecondary>
+            <AvatarSuccess
+              sx={{
+                mr: 2
+              }}
+              variant="rounded"
+            >
+              <AssignmentTurnedInTwoToneIcon fontSize="large" />
+            </AvatarSuccess>
+            <Box>
+              <Typography variant="h1">5</Typography>
+              <TypographySecondary variant="subtitle2" noWrap>
+                Most Read Articles
+              </TypographySecondary>
+            </Box>
           </Box>
-        </Box>
-        <Box
-          display="flex"
-          sx={{
-            px: 2,
-            pb: 3
-          }}
-          alignItems="center"
-        >
-          <AvatarError
+          <Box
+            display="flex"
             sx={{
-              mr: 2
+              px: 2,
+              pb: 3
             }}
-            variant="rounded"
+            alignItems="center"
           >
-            <CancelPresentationTwoToneIcon fontSize="large" />
-          </AvatarError>
-          <Box>
-            <Typography variant="h1">5</Typography>
-            <TypographySecondary variant="subtitle2" noWrap>
-              tasks closed
-            </TypographySecondary>
+            <AvatarError
+              sx={{
+                mr: 2
+              }}
+              variant="rounded"
+            >
+              <CancelPresentationTwoToneIcon fontSize="large" />
+            </AvatarError>
+            <Box>
+              <Typography variant="h1">25</Typography>
+              <TypographySecondary variant="subtitle2" noWrap>
+                Average Read Articles
+              </TypographySecondary>
+            </Box>
           </Box>
-        </Box>
-        <Box pt={3}>
-          <LinearProgressWrapper
-            value={73}
-            color="primary"
-            variant="determinate"
-          />
-        </Box>
-      </CardContent>
-    </RootWrapper>
-  );
-}
-
-export default Performance;
+          <Box pt={3}>
+            <LinearProgressWrapper
+              value={73}
+              color="primary"
+              variant="determinate"
+            />
+          </Box>
+        </CardContent>
+      </RootWrapper>
+    );
+  }
+  
+  export default Performance;
+  
